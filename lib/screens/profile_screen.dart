e5b45fade269874868502b8185bb3e506c45a7e1
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2025-02-05 12:50:17 +0800

    profile done, small bugs left

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -10,6 +10,25 @@ class ProfileScreen extends StatelessWidget {
     const double appBarHeight = 170;
     const double profileHeight = 140;
     const double top = appBarHeight - (profileHeight / 2);
+    String medal = 'assets/images/medals/';
+
+    switch (data['rank']) {
+      case 'Donor Ambassador':
+        medal = '${medal}ambassador.png';
+        break;
+      case 'Regular Donor':
+        medal = '${medal}regular.png';
+        break;
+      case 'Top Donor':
+        medal = '${medal}top.png';
+        break;
+      case 'Occasional Donor':
+        medal = '${medal}occasional.png';
+        break;
+      case 'Donor Leader':
+        medal = '${medal}leader.png';
+        break;
+    }
 
     return Scaffold(
       appBar: PreferredSize(
@@ -86,102 +105,111 @@ class ProfileScreen extends StatelessWidget {
       ),
 
       // FIXME: top padding bugged because of appbar preferred size
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 1st section
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ProfileField(
-                        fieldName: 'Full Name:', data: data['fullname']),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ProfileField(
-                        fieldName: 'Blood Type:', data: data['blood type']),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ProfileField(
-                        fieldName: 'Age:', data: data['age'].toString()),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // 1st section
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ProfileField(
+                          fieldName: 'Full Name:', data: data['fullname']),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ProfileField(
+                          fieldName: 'Blood Type:', data: data['blood type']),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ProfileField(
+                          fieldName: 'Age:', data: data['age'].toString()),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ProfileField(
+                          fieldName: 'Donations:',
+                          data: data['donations'].toString()),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ProfileField(
+                          fieldName: 'Last Donation:',
+                          data: data['last donation']),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              // 2nd section
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ProfileField(fieldName: 'Address:', data: data['address'])
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ProfileField(fieldName: 'Phone:', data: data['phone']),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      ProfileField(fieldName: 'Email:', data: data['email'])
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              // 3rd section
+              const Text(
+                'Your Current Rank',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(data['rank']),
+              Center(
+                child: Image.asset(
+                  medal,
+                  width: 200,
+                  fit: BoxFit.contain,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ProfileField(
-                        fieldName: 'Donations:',
-                        data: data['donations'].toString()),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ProfileField(
-                        fieldName: 'Last Donation:',
-                        data: data['last donation']),
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Divider(
-              color: Colors.black12,
-              thickness: 1,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            // 2nd section
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    ProfileField(fieldName: 'Address:', data: data['address'])
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    ProfileField(fieldName: 'Phone:', data: data['phone']),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    ProfileField(fieldName: 'Email:', data: data['email'])
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Divider(
-              color: Colors.black12,
-              thickness: 1,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            // 3rd section
-            const Text(
-              'Your Current Rank',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Text(data['rank']),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
